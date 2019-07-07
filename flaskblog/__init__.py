cbc7b2cd90f03339462444d2330e1b1b87178c50
JamesMutahi <mutahijames0@gmail.com>
2019-07-07 15:35:30 +0300

    initialize Bcrypt LoginManager and mail

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -13,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://jim:12139@localho
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'

c668b4d50277e5218cf0c3f9a772ec8ea1706272
JamesMutahi <mutahijames0@gmail.com>
2019-07-08 00:45:59 +0300

    deploy to heroku

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,7 +10,7 @@ from flask_simplemde import SimpleMDE
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://jim:12139@localhost/blog'
 # DATABASE_URI='postgresql+psycopg2://jim:12139@localhost/blog'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
